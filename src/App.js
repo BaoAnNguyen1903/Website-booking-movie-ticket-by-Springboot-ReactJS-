71947c3233c74a18a10e5a31ec7c10beecf34864
An Nguyen <anndb1903@gmail.com>
2024-12-22 19:20:12 +0700

    git

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import UserList from './components/User/userList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/login';
+import ViewDetail from './components/User/viewDetail';
 
 function App() {
   return (
-    <div className="App">
-      <div>
- 
-      <UserList></UserList>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="userList" element={<UserList></UserList>}>
+          <Route path="viewDetail" element={<ViewDetail></ViewDetail>} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 export default App;
