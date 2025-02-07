8f33a8b10fd622abd6c79598d4e06a08e6b2d090
An Nguyen <anndb1903@gmail.com>
2025-02-07 15:01:47 +0700

    #91. Responsive

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification } from "antd";
+import { Button, Input, Form, notification, Row, Col } from "antd";
 import { registerUserAPI } from "../services/api.service";
 import { useNavigate } from "react-router-dom";
 
@@ -30,84 +30,99 @@ const RegisterPage = () => {
       form={form}
       layout="vertical"
       onFinish={onFinish}
+      style={{ margin: "10px" }}
       //   onFinishFailed={onFinishFailed}
     >
-      <div
-        style={{
-          margin: "50px"
-        }}
-      >
-        <Form.Item
-          label="Full Name"
-          name="fullName"
-          rules={[
-            {
-              required: true,
-              message: "Please input your full name!"
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Please input your email!"
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password" // giống thẻ spanspan
-          name="password"
-          rules={[
-            {
-              required: true, // dấu * đỏ thể hiện bắt buộc phải nhập vào
-              message: "Please input your password!"
-            }
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          label="Phone Number"
-          name="phone"
-          rules={[
-            {
-              required: true,
-              pattern: new RegExp(/\d+/g),
-              message: "Wrong format!"
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <div>
-          <Button onClick={() => form.submit()} type="primary">
-            Register
-          </Button>
-
-          <Button
-            onClick={() => {
-              form.setFieldsValue({
-                email: "anndbdeptrai@gmail.com",
-                fullName: "an dep trai"
-              });
-              console.log(">>> check form: ");
-            }}
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Full Name"
+            name="fullName"
+            rules={[
+              {
+                required: true,
+                message: "Please input your full name!"
+              }
+            ]}
           >
-            Test
-          </Button>
-        </div>
-      </div>
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!"
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>  
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Password" // giống thẻ spanspan
+            name="password"
+            rules={[
+              {
+                required: true, // dấu * đỏ thể hiện bắt buộc phải nhập vào
+                message: "Please input your password!"
+              }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Phone Number"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                pattern: new RegExp(/\d+/g),
+                message: "Wrong format!"
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <div>
+            <Button onClick={() => form.submit()} type="primary">
+              Register
+            </Button>
+
+            <Button
+              onClick={() => {
+                form.setFieldsValue({
+                  email: "anndbdeptrai@gmail.com",
+                  fullName: "an dep trai"
+                });
+                console.log(">>> check form: ");
+              }}
+            >
+              Test
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Form>
   );
 };
