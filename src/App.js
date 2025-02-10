a201e4fe8b0d0b3dcae592118065fa764ee9b213
An Nguyen <anndb1903@gmail.com>
2025-02-10 09:24:07 +0700

    #100

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Header from './components/layout/header';
 import Footer from './components/layout/footer';
+import { getAccountAPI } from './services/api.service';
+import { AuthContext } from "../context/auth.context";
 
 const App = () => {
+  const { setUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []) // ngoac [] chi chay 1 lan`
+
+  const fetchUserInfo = async() => {
+    const res = await getAccountAPI();
+    if (res.data){
+      setUser(res.data.user)
+    }
+  }
   return (
     <>
     <Header />
